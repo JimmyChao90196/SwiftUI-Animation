c4cb1cb4e56f799263f7dbd8530bc04f3041e9ef
Jimmy <myname90196@gmail.com>
2024-03-17 11:05:09 +0800

    Initial Commit

---
+++ SwiftUI-Animation/SwiftUI_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AnimationApp.swift
+//  SwiftUI-Animation
+//
+//  Created by JimmyChao on 2024/3/17.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
